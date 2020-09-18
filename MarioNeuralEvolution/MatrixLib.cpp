290d9b89f7cb21dac8f16f381c76f54346c46063
Danny <38340314+DanielKhali1@users.noreply.github.com>
2020-09-18 15:20:27 -0500

    pls god kill me

---
+++ MarioNeuralEvolution/MatrixLib.cpp
@@ -0,0 +1,71 @@
+#include "MatrixLib.h"
+#include <iostream>
+
+MatrixLib::MatrixLib(unsigned int width, unsigned int height)
+{
+	this->width = width; //componentsof
+	this->height = height; //numVectors
+	matrix = (float**)malloc(sizeof(float*) * height);
+	for (unsigned int i = 0; i < height; i++) {
+		*(matrix + i) = (float*)malloc(sizeof(float) * width) ; //*(*(matrix + i) +j ) == matrix[i][j]
+		for (unsigned int j = 0; j < width; j++) {
+			//assign
+			//*(*(matrix + i) +j ) == matrix[i][j]
+			*(*(matrix + i) + j) = 2.0;
+		}
+	}
+}
+float MatrixLib::DotProduct(MatrixLib* mult) { //Frobenius inner product... I think
+	float totals = 0;
+	unsigned int minRows = 0;
+	unsigned int minComp = 0;
+	float** multMat = mult->matrix;
+	if (this->height < mult->height ) {
+		minRows = this->height;
+	} else {
+		minRows = mult->height;
+	}
+	if (this->width < mult->width) {
+		minComp = this->width;
+	} else {
+		minComp = mult->width;
+	}
+	for (unsigned int i = 0; i < minRows; i++) {
+		for (unsigned int j = 0; j < minComp; j++) {
+			float a = 0;
+			float b = 0;
+			totals += *(*(multMat + i) + j) * *(*(matrix + i) + j);
+		}
+	}
+	return totals;
+}
+void MatrixLib::mult(float coef){
+	for (unsigned int i = 0; i < this->height; i++) {
+		for (unsigned int j = 0; j < this->width; j++) {
+			*(*(matrix + i) + j) = *(*(matrix + i) + j) * coef;
+		}
+	}
+}
+void MatrixLib::addition(MatrixLib* add) {
+	float totals = 0;
+	unsigned int minRows = 0;
+	unsigned int minComp = 0;
+	float** addMat = add->matrix;
+	if (this->height < add->height) {
+		minRows = this->height;
+	}
+	else {
+		minRows = add->height;
+	}
+	if (this->width < add->width) {
+		minComp = this->width;
+	}
+	else {
+		minComp = add->width;
+	}
+	for (unsigned int i = 0; i < minRows; i++) {
+		for (unsigned int j = 0; j < minComp; j++) {
+			*(*(matrix + i) + j) = *(*(addMat + i) + j) + *(*(matrix + i) + j);
+		}
+	}
+}
